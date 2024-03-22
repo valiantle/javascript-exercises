b8206ee9150419694d3a698033b326e117524569
Niels Hoppe <mail@nielshoppe.de>
2024-03-22 11:03:04 +0100

    Add Array exercises

---
+++ arrays.mjs
@@ -0,0 +1,40 @@
+import { isEven, isOdd } from './basics.mjs'
+
+/**
+ * Schreibe eine Funktion, die ein Array von Zahlen so filtert, dass nur die
+ * ungeraden Zahlen im Ergebnis enthalten sind.
+ * 
+ * Optional: Verwende deine Funktion isOdd aus den basics
+ * 
+ * @param {[number]} numbers 
+ */
+export function filterOdd (numbers) {
+
+    // ...
+}
+
+/**
+ * Schreibe eine Funktion, die ein Array von Zahlen so filtert, dass nur die
+ * geraden Zahlen im Ergebnis enthalten sind.
+ * 
+ * Optional: Verwende deine Funktion isEven aus den basics
+ * 
+ * @param {[numbers]} numbers 
+ */
+export function filterEven (numbers) {
+
+    // ...
+}
+
+/**
+ * Schreibe eine Funktion, die entweder eine einzelne Zahl oder ein Array von
+ * Zahlen akzeptiert und als Ergebnis das Quadrat der Zahl bzw. ein Array mit
+ * den Quadraten aller Zahlen zurück gibt.
+ * 
+ * @param {number|[number]} numbers 
+ * @returns {number|[number]}
+ */
+export function square (numbers) {
+
+    // ...
+}
