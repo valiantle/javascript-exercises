be3fad944f9167629b22c741058fd8a04771661b
Niels Hoppe <mail@nielshoppe.de>
2024-03-28 15:29:08 +0100

    Add example, update docstrings

--- countries.mjs
+++ countries.mjs
@@ -9,8 +9,8 @@ const countries = require("./countries.json");
 /**
  * Schreibe eine Funktion, die ein Land daraufhin prüft, ob es in Europa liegt.
  * 
- * @param {*} country 
- * @returns 
+ * @param {object} country 
+ * @returns {boolean}
  */
 export function isEurope (country) {
 
@@ -20,8 +20,8 @@ export function isEurope (country) {
 /**
  * Schreibe eine Funktion, die den official name eines Landes zurückgibt.
  * 
- * @param {*} country 
- * @returns 
+ * @param {object} country 
+ * @returns {string}
  */
 export function officialName(country) {
     
@@ -32,8 +32,8 @@ export function officialName(country) {
  * Schreibe eine Funktion, die alle Namen (common, official, native, ...) eines
  * Landes in einer Liste zurück gibt.
  * 
- * @param {*} country 
- * @returns 
+ * @param {object} country 
+ * @returns {[string]}
  */
 export function allNames (country) {
 
@@ -44,6 +44,28 @@ export function allNames (country) {
  * Schreibe eine Funktion, die ein Objekt nach einer Liste von keys filtert und
  * ein neues Objekt zurückgibt, welches nur die angegebenen keys sowie die
  * dazugehörigen Werte enthält.
+ * 
+ * Beispiel:
+ * 
+ * const obj = {
+ *     apfel: 10,
+ *     banane: 20,
+ *     kiwi: 15,
+ *     maracuja: 42
+ * }
+ * 
+ * filterKeys(obj, ["apfel", "maracuja"])
+ * 
+ * Ergebnis:
+ * 
+ * {
+ *     apfel: 10,
+ *     maracuja: 42
+ * }
+ * 
+ * @param {object} obj 
+ * @param {[*]} keys 
+ * @returns {object}
  * 
  * @param {*} obj 
  * @param {*} keys 
