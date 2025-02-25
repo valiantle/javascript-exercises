d304f583525d89d4df7389f69e19f9c0a3ae6012
Niels Hoppe <mail@nielshoppe.de>
2025-02-25 13:54:51 +0100

    Add tests for temperature conversion and even/odd number checks

--- basics.test.js
+++ basics.test.js
@@ -1,6 +1,7 @@
 import { afterAll, expect, test, vi } from "vitest"
 
-import { add, subtract, multiply, divide, umsatzsteuer } from "./basics.mjs"
+import { isEven, isOdd } from "./basics.mjs"
+import { add, subtract, multiply, divide, umsatzsteuer, inFahrenheit, inCelsius, jahreszeit } from "./basics.mjs"
 import { area, fizzbuzz, fibonacci } from "./basics.mjs"
 
 test('Adding 5 to 3 gives 8', () => {
@@ -17,6 +18,77 @@ test('Multiplying 4 by 6 gives 24', () => {
 
 test('Dividing 12 by 4 gives 3', () => {
     expect(divide(12, 4)).toBe(3)
+})
+
+test('Converting 0°C to Fahrenheit is 32°F', () => {
+    expect(inFahrenheit(0)).toBe(32)
+})
+
+test('Converting -40°C to Fahrenheit is -40°F', () => {
+    expect(inFahrenheit(-40)).toBe(-40)
+})
+
+test('Converting 32°F to Celsius is 0°C', () => {
+    expect(inCelsius(32)).toBe(0)
+})
+
+test('Converting -40°F to Celsius is -40°C', () => {
+    expect(inCelsius(-40)).toBe(-40)
+})
+
+test('0, 2, 4, 8, 44 are even', () => {
+    expect(isEven(0)).toBe(true)
+    expect(isEven(2)).toBe(true)
+    expect(isEven(4)).toBe(true)
+    expect(isEven(8)).toBe(true)
+    expect(isEven(44)).toBe(true)
+})
+
+test('1, 3, 7, 11, 55 are not even', () => {
+    expect(isEven(1)).toBe(false)
+    expect(isEven(3)).toBe(false)
+    expect(isEven(7)).toBe(false)
+    expect(isEven(11)).toBe(false)
+    expect(isEven(55)).toBe(false)
+})
+
+test('1, 3, 5, 9, 77 are odd', () => {
+    expect(isOdd(1)).toBe(true)
+    expect(isOdd(3)).toBe(true)
+    expect(isOdd(5)).toBe(true)
+    expect(isOdd(9)).toBe(true)
+    expect(isOdd(77)).toBe(true)
+})
+
+test('0, 2, 6, 12, 88 are not odd', () => {
+    expect(isOdd(0)).toBe(false)
+    expect(isOdd(2)).toBe(false)
+    expect(isOdd(6)).toBe(false)
+    expect(isOdd(12)).toBe(false)
+    expect(isOdd(88)).toBe(false)
+})
+
+test('Frühlingsmonate', () => {
+    expect(jahreszeit(3)).toBe('Frühling')
+    expect(jahreszeit(4)).toBe('Frühling')
+    expect(jahreszeit(5)).toBe('Frühling')
+})
+test('Sommermonate', () => {
+    expect(jahreszeit(6)).toBe('Sommer')
+    expect(jahreszeit(7)).toBe('Sommer')
+    expect(jahreszeit(8)).toBe('Sommer')
+})
+
+test('Herbstmonate', () => {
+    expect(jahreszeit(9)).toBe('Herbst')
+    expect(jahreszeit(10)).toBe('Herbst')
+    expect(jahreszeit(11)).toBe('Herbst')
+})
+
+test('Wintermonate', () => {
+    expect(jahreszeit(12)).toBe('Winter')
+    expect(jahreszeit(1)).toBe('Winter')
+    expect(jahreszeit(2)).toBe('Winter')
 })
 
 test('Umsatzsteuer 2005 (18k)', () => {
