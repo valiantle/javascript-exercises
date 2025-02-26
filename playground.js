a15ba9616061abf744dfca11c7f861cf524d0f8a
Niels Hoppe <mail@nielshoppe.de>
2025-02-26 10:22:50 +0100

    Add initial setup for playground with example objects and launch configuration

---
+++ playground.js
@@ -0,0 +1,27 @@
+/**
+ * Erzeuge ein Objekt namens `auto1` mit den Eigenschaften:
+ * - marke: 'VW'
+ * - modell: 'Golf'
+ * - baujahr: 2010
+ * - ps: 140
+ * - farbe: 'blau'
+ */
+
+// ... dein Code hier
+
+/**
+ * Lass dir die Eigenschaften des Objekts `auto1` im Terminal ausgeben.
+ */
+
+// ... dein Code hier
+
+/**
+ * Erzeuge ein weiteres Objekt namens `auto2` mit den Eigenschaften:
+ * - marke: 'Audi'
+ * - modell: 'A4'
+ * - baujahr: 2015
+ * - ps: 190
+ * - farbe: 'grau'
+ */
+
+// ... dein Code hier
